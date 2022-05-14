66ac9e17834affca577a46aae6e74d91e7ab5ff1
ananyabarthakur <66308814+ananyabarthakur@users.noreply.github.com>
2022-05-14 14:53:22 -0400

    Add files via upload

---
+++ fcn_data (1).py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+import glob
+import matplotlib.image as mpimg
+class FCNDataset(torch.utils.data.Dataset):
+    def __init__(self, train = True):
+        self.x = sorted(glob.glob('./Augmented_Data/Slide_*/*'))
+
+        self.y = sorted(glob.glob('./Augmented_Data/GT_*/*'))
+        
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, idx):
+        x = np.transpose(mpimg.imread(self.x[idx]),[2,1,0])
+        y = np.transpose(mpimg.imread(self.y[idx]).reshape(512,512,1),[2,1,0])
+        return torch.tensor(x).float(), torch.tensor(y).float()
