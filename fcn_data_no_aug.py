5df8ebfd90c98962b80fc85720d7d0a01d60c9ff
ananyabarthakur <66308814+ananyabarthakur@users.noreply.github.com>
2022-05-15 16:17:00 -0400

    Add files via upload

---
+++ fcn_data_no_aug.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch
+import glob
+import matplotlib.image as mpimg
+class FCN_No_Aug_Dataset(torch.utils.data.Dataset):
+    def __init__(self, train = True):
+        self.x = sorted(glob.glob('./TNBC_NucleiSegmentation/Slide_*/*'))
+        self.x.remove('./TNBC_NucleiSegmentation/Slide_09/09_2.png')
+        self.x.remove('./TNBC_NucleiSegmentation/Slide_09/09_3.png')
+        self.x.remove('./TNBC_NucleiSegmentation/Slide_09/09_4.png')
+      
+        self.y = sorted(glob.glob('./TNBC_NucleiSegmentation/GT_*/*'))
+        self.y.remove('./TNBC_NucleiSegmentation/GT_09/09_2.png')
+        self.y.remove('./TNBC_NucleiSegmentation/GT_09/09_3.png')
+        self.y.remove('./TNBC_NucleiSegmentation/GT_09/09_4.png')
+
+
+    def __len__(self):
+        return len(self.x)
+   
+    def __getitem__(self, idx):
+        x = np.transpose(mpimg.imread(self.x[idx])[:,:,:3],[2,1,0])
+        y = np.transpose(mpimg.imread(self.y[idx]).reshape(512,512,1),[2,1,0])
+        return torch.tensor(x).float(), torch.tensor(y).float()
---
+++ fcn_train_no_aug.py
@@ -0,0 +1,133 @@
+import numpy as np
+import sklearn
+from torch.utils.data import DataLoader, Subset
+from sklearn.model_selection import train_test_split
+
+from fcn_no_aug_data import *
+from fcn import *
+
+import wandb
+
+wandb.login()
+
+num_epochs = 20
+test_split = 0.3
+batch_size = 8
+SEED = 42
+lr = 0.005
+
+data = FCN_No_Aug_Dataset()
+
+print(len(data))
+
+# generate indices: instead of the actual data we pass in integers instead
+train_indices, test_indices, _, _ = train_test_split(
+    range(len(data)),
+    data.y,
+    test_size=test_split,
+    random_state=SEED
+)
+
+# generate subset based on indices
+train = Subset(data, train_indices)
+test = Subset(data, test_indices)
+print(len(train))
+print(len(test))
+import numpy as np
+import sklearn
+from torch.utils.data import DataLoader, Subset
+from sklearn.model_selection import train_test_split
+
+from fcn_data import *
+#from ResNet import *
+from fcn import *
+
+import wandb
+
+wandb.login()
+
+num_epochs = 20
+test_split = 0.3
+batch_size = 8
+SEED = 42
+lr = 0.005
+
+data = FCNDataset()
+
+print(len(data))
+
+# generate indices: instead of the actual data we pass in integers instead
+train_indices, test_indices, _, _ = train_test_split(
+    range(len(data)),
+    data.y,
+    test_size=test_split,
+    random_state=SEED
+)
+
+# generate subset based on indices
+train = Subset(data, train_indices)
+test = Subset(data, test_indices)
+print(len(train))
+print(len(test))
+# MODEL
+#model = model(True)
+model = FCN_res101(4,8)
+# LOSS
+#criterion = nn.BCELoss()
+loss = torch.nn.CrossEntropyLoss()
+
+# OPTIMIZER
+optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+
+# WANDB
+config_dict = dict(
+    epochs = num_epochs,
+    batch_size = batch_size,
+    dataset_len = len(data),
+    test_split = test_split,
+    lr = lr
+)
+wandb.init(project="BIA final project No Augmentation + FCN", entity="ananyab",config=config_dict)
+
+# TRAIN AND TEST LOOP
+tr_loss = []
+val_loss = []
+for epoch in range(num_epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(train_generator, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        #print("check target tensor: ", outputs.unsqueeze(1).type(torch.FloatTensor))
+        #loss = criterion(outputs.unsqueeze(1).type(torch.FloatTensor), labels.unsqueeze(1).type(torch.FloatTensor))
+        #loss.backward()
+        loss_ce = loss(outputs, labels.squeeze(1).type(torch.LongTensor))
+        loss_ce.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss_ce.item()
+    epoch_train_loss = running_loss / (train_size//batch_size+1)
+    tr_loss.append(epoch_train_loss)
+    print("Epoch:", epoch, "\tTrain Loss:", epoch_train_loss)
+    wandb.log({'Epoch Num': epoch+1, 'Train loss': epoch_train_loss})
+
+    test_loss = 0.0
+    for i, data in enumerate(test_generator, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        #loss = criterion(outputs.unsqueeze(1).type(torch.FloatTensor), labels.unsqueeze(1).type(torch.FloatTensor))
+        loss_ce = loss(outputs, labels.squeeze(1).type(torch.LongTensor))
+        test_loss += loss_ce.item()
+    epoch_val_loss = test_loss / (test_size//batch_size+1)
+    val_loss.append(epoch_val_loss)
+    print("Test Loss:", epoch_val_loss)
+    wandb.log({'Epoch Num': epoch+1, 'Test loss': epoch_val_loss})
